9bb61d93700cecb33e55112340919aabf67f2c9b
JiangYan0426 <2571560168@qq.com>
2021-10-11 10:38:57 +0800

    项目目录搭建 styled-components、reset.css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Globalstyle } from "./style";
+
+
+function App() {
+  return (
+    <div className='dell'>
+      Hello World
+      <Globalstyle/>
+    </div>
+  );
+}
+
+export default App;
