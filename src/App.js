efac3d2b4b1927163bd37ef9dd53db99f1306f5e
JiangYan0426 <2571560168@qq.com>
2021-10-11 15:46:15 +0800

    用style-components完成Header组件布局

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Globalstyle } from "./style";
-
-
-function App() {
-  return (
-    <div className='dell'>
-      Hello World
-      <Globalstyle/>
-    </div>
-  );
+import React, { Component } from 'react';
+import { Globalstyled } from "./style";
+import Header from './common/header';
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        {/* injectGlobal问题的解决，不可删 */}
+        <Globalstyled/> 
+      </div>
+    )
+  }
 }
-
-export default App;
