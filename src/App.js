badc43f8e06f6c7645be60469900b180ce65caab
JiangYan0426 <2571560168@qq.com>
2021-10-21 17:07:06 +0800

    什么是路由，如何在React中使用路由功能/首页组件的拆分

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux';
+import {BrowserRouter,Route} from 'react-router-dom';
 import { Globalstyled } from "./style";
 import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import store from './store';
 export default class App extends Component {
   render() {
@@ -10,6 +13,11 @@ export default class App extends Component {
         {/* 意思是Provider里面的都可以用store */}
         <Provider store={store}>
           <Header/>
+          <BrowserRouter>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/detail' exact component={Detail}></Route>
+
+          </BrowserRouter>
           {/* injectGlobal问题的解决，不可删 */}
           <Globalstyled/> 
         </Provider>
